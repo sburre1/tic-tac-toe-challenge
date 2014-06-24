8452ae05d3da84964dc97676fa1d46fdb87a73d8
sburre1 <sandra.davis.github@gmail.com>
2014-06-23 19:47:59 -0500

    First unit test and url mapping, dummy view

--- tic_tac_toe/urls.py
+++ tic_tac_toe/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tic_tac_toe.views.home', name='home'),
+    url(r'^$', 'game.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
